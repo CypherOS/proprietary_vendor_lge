29e822dd8df0e9726dfe2ad60c8215dcc9482fdf
LorDClockaN <davor@losinj.com>
2014-11-06 16:29:14 +0100

    Delete overlay declaration to fix build
    Change-Id: I79509b718ac1602eedc0d47cf0483666c51e14ed

--- hammerhead/hammerhead-vendor.mk
+++ hammerhead/hammerhead-vendor.mk
@@ -26,9 +26,6 @@ PRODUCT_PACKAGES := \
 PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/lge/hammerhead/overlay
-
 # Apps
 PRODUCT_PACKAGES += \
     OmaDmclient \
