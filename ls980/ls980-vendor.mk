be2ee508eff371fc60566fa475468830493299b8
drgravy <drg113001@gmail.com>
2014-11-23 22:05:32 -0600

    Revert "vendor_lge: Update to the current status from @Drgravy"
    Change-Id: I4b7af3a2bacf6697050f704e162b190ef2d9236a

--- ls980/ls980-vendor.mk
+++ ls980/ls980-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The Android Open-Source Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,10 +21,6 @@ PRODUCT_PACKAGES := \
     MagicSmokeWallpapers \
     VisualizationWallpapers \
     librs_jni
-
-# Apps
-PRODUCT_PACKAGES += \
-    TimeService
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \
