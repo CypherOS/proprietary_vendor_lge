50a4504c25ee2d54df0afbfb7bf976dc62dd094c
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-28 03:35:25 -0600

    g3: clean up blobs
    * remove unused blobs
    * bring liblgderp to common
    
    Change-Id: I76ea0e38bf81df06b7ca5ff6ed1b5ce718d06179

--- d852/d852-vendor-blobs.mk
+++ d852/d852-vendor-blobs.mk
@@ -56,7 +56,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/d852/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/lge/d852/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/lge/d852/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-    vendor/lge/d852/proprietary/lib/liblgderp.so:system/lib/liblgderp.so \
     vendor/lge/d852/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/lge/d852/proprietary/lib/libvss_common_idl.so:system/lib/libvss_common_idl.so \
     vendor/lge/d852/proprietary/lib/libvss_common_iface.so:system/lib/libvss_common_iface.so \
