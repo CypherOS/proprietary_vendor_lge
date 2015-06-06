2b5382722fc2ccc984000b34967a1d8307614fdc
Rashed Abdel-Tawab <rashed@linux.com>
2015-06-06 11:34:55 -0500

    g2: add librmp and librpmb
    Change-Id: I9cf65e7750cf097f0ecc28b2725a0f22b450a81f

--- g2-common/g2-common-vendor-blobs.mk
+++ g2-common/g2-common-vendor-blobs.mk
@@ -287,6 +287,8 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g2-common/proprietary/vendor/lib/libquipc_ulp_adapter.so:system/vendor/lib/libquipc_ulp_adapter.so \
     vendor/lge/g2-common/proprietary/vendor/lib/libril-qc-qmi-1.so:system/vendor/lib/libril-qc-qmi-1.so \
     vendor/lge/g2-common/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
+    vendor/lge/g2-common/proprietary/vendor/lib/librmp.so:system/vendor/lib/librmp.so \
+    vendor/lge/g2-common/proprietary/vendor/lib/librpmb.so:system/vendor/lib/librpmb.so \
     vendor/lge/g2-common/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/lge/g2-common/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/lge/g2-common/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
