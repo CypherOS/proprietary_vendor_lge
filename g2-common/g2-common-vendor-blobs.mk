4e27f11560697222e63851743b543f1cd4375b48
Rashed Abdel-Tawab <rashed@linux.com>
2015-06-06 11:34:55 -0500

    g2: Remove google.widevine.software, this is built by the system
    Change-Id: Ia7549874c1927d71f00698efff3f26c28bca228d

--- g2-common/g2-common-vendor-blobs.mk
+++ g2-common/g2-common-vendor-blobs.mk
@@ -76,7 +76,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g2-common/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
     vendor/lge/g2-common/proprietary/etc/permissions/qcnvitems.xml:system/etc/permissions/qcnvitems.xml \
     vendor/lge/g2-common/proprietary/etc/permissions/com.qualcomm.location.xml:system/etc/permissions/com.qualcomm.location.xml \
-    vendor/lge/g2-common/proprietary/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
     vendor/lge/g2-common/proprietary/lib/hw/camera.msm8974.so:system/lib/hw/camera.msm8974.so \
     vendor/lge/g2-common/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/lge/g2-common/proprietary/lib/libchromatix_imx135_liveshot.so:system/lib/libchromatix_imx135_liveshot.so \
