8a603b621c4cd5687f3d3ec2fef4fd42d73b420d
pavlaras <theodoroupaulos@gmail.com>
2018-01-26 20:39:29 +0100

    Revert "g4: Add the call to build the live wallpaper packages and persmissions"
    This reverts commit 72e35a52c93611066b4050d23f5ee12611eac12b.
    
    Change-Id: I2f9a18c199ee1160cfa79b0708ac0385a8428d24

--- g4-common/g4-common-vendor.mk
+++ g4-common/g4-common-vendor.mk
@@ -821,15 +821,3 @@ PRODUCT_PACKAGES += \
     libqmiservices \
     libqmi_cci \
     libqmi_client_helper
-
-# Live wallpaper packages
-PRODUCT_PACKAGES := \
-	LiveWallpapers \
-	LiveWallpapersPicker \
-	MagicSmokeWallpapers \
-	VisualizationWallpapers \
-	librs_jni
-
-# Publish that we support the live wallpaper feature.
-PRODUCT_COPY_FILES := \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
